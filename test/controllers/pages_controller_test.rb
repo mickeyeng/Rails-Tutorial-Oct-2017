a3907846d6e6db97924629f5ff2307a63e4fb5c2
Mickey English <mickeyenglish@mickeys-mbp.mynet>
2017-10-22 01:11:30 +0100

    add a full title helper

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,21 +4,21 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get pages_index_url
     assert_response :success
-    assert_select "title", "Home Page"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
 
   test "should get about" do
   	get pages_about_url
   	assert_response :success
-  	assert_select "title", "About Page"
+  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 
   test "should get contact" do
   	get pages_contact_url
   	assert_response :success
-  	assert_select "title", "Contact Page"
+  	 assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 
